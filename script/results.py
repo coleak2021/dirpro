e807bd1905cb5a85ec8c0fd64958f68974b5595e
coleak2021 <1209506146@qq.com>
2023-09-24 22:42:55 +0800

    dirpro v1.2

--- script/results.py
+++ script/results.py
@@ -45,5 +45,4 @@ def __Results(rooturl,ret):
         if result['length'] != maxLength:
             __log(f'{result["status_code"]:<6}{result["length"]:<7}{result["url"]}')
     f.close()
-    ret=[]
     return t
